01f68df80808b64432c0f58ed94cdd3a87502de8
Justin Myre <justin.myre@gmail.com>
2017-03-16 15:39:51 -0700

    'Practice'

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'WelcomeController');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/book/{title?}', function($title=null) {
+	if($title == null) {
+		return "You need to be more specific";
+	}
+    return "You want to view ".$title;
 });
+
+if(config('app.env')== 'local') {
+	Route::get('/logs', function(){ });
+}
+
+Route::get('/practice', function() {
+    echo config('mail.driver');
+});
+
+Route::any('/practice/{n?}', 'PracticeController@index');
